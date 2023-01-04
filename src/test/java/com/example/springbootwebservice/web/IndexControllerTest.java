f694a89d47b9663530a2084d103c4452335a4473
kirby <kirby@socar.kr>
2023-01-04 21:17:28 +0900

    chore: 오타 수정

--- src/test/java/com/example/springbootwebservice/web/IndexControllerTest.java
+++ src/test/java/com/example/springbootwebservice/web/IndexControllerTest.java
@@ -18,7 +18,7 @@ public class IndexControllerTest {
     private TestRestTemplate restTemplate;
 
     @Test
-    public void 메인페이지_가로딩(){
+    public void 메인페이지_로딩() {
         // when
         String body = this.restTemplate.getForObject("/", String.class);
 
