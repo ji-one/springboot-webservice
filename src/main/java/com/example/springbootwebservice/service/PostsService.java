81f362ee1245817bc4377e8fad8bb72d658cb751
kirby <kirby@socar.kr>
2023-01-04 21:52:52 +0900

    feat: 전체 조회 화면 생성

--- src/main/java/com/example/springbootwebservice/service/PostsService.java
+++ src/main/java/com/example/springbootwebservice/service/PostsService.java
@@ -2,13 +2,16 @@ package com.example.springbootwebservice.service;
 
 import com.example.springbootwebservice.domain.posts.Posts;
 import com.example.springbootwebservice.domain.posts.PostsRepository;
+import com.example.springbootwebservice.web.dto.PostsListResponseDto;
 import com.example.springbootwebservice.web.dto.PostsResponseDto;
 import com.example.springbootwebservice.web.dto.PostsSaveRequestDto;
 import com.example.springbootwebservice.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
-import javax.transaction.Transactional;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Service
@@ -34,5 +37,12 @@ public class PostsService {
         Posts entity = postsRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id=" + id));
 
         return new PostsResponseDto(entity);
+    }
+
+    @Transactional(readOnly = true) // 트랜잭션 범위는 유지하되, 조회 기능만 남겨두어 조회 속도가 개선되기 때문에 등록, 수정, 삭제 기능이 없는 서비스 메소드에 사용하는 것을 추천
+    public List<PostsListResponseDto> findAllDesc() {
+        return postsRepository.findAllDesc().stream()
+                .map(PostsListResponseDto::new)
+                .collect(Collectors.toList());
     }
 }
--- src/main/java/com/example/springbootwebservice/web/dto/IndexController.java
+++ src/main/java/com/example/springbootwebservice/web/dto/IndexController.java
@@ -1,18 +1,25 @@
 package com.example.springbootwebservice.web.dto;
 
+import com.example.springbootwebservice.domain.posts.PostsRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+@RequiredArgsConstructor
 @Controller
 public class IndexController {
 
+    private final PostsRepository postsService;
+
     @GetMapping("/")
-    public String index() {
+    public String index(Model model) {
+        model.addAttribute("posts", postsService.findAllDesc());
         return "index";
     }
 
     @GetMapping("/posts/save")
-    public String postsSave(){
+    public String postsSave() {
         return "posts-save";
     }
 
