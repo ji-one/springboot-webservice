9527bdbe432d3fdf42bb5635520bb74c094fad16
kirby <kirby@socar.kr>
2023-01-12 21:05:58 +0900

    fix: WebSecurityConfigurerAdapter deprecated 이슈 수정

--- src/main/java/com/example/springbootwebservice/config/auth/SecurityConfig.java
+++ src/main/java/com/example/springbootwebservice/config/auth/SecurityConfig.java
@@ -2,18 +2,19 @@ package com.example.springbootwebservice.config.auth;
 
 import com.example.springbootwebservice.domain.user.Role;
 import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.SecurityFilterChain;
 
 @RequiredArgsConstructor
 @EnableWebSecurity // Spring Security 설정 활성화
-public class SecurityConfig extends WebSecurityConfigurerAdapter {
+public class SecurityConfig {
 
     private final CustomOAuth2UserService customOAuth2UserService;
 
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
+    @Bean
+    protected SecurityFilterChain configure(HttpSecurity http) throws Exception {
         http.csrf().disable()
                 .headers().frameOptions().disable() // h2-console 화면을 사용하기 위해 해당 옵션들을 disable 한다.
                 .and()
@@ -30,6 +31,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .oauth2Login() // OAuth2 로그인 기능에 대한 여러 설정의 진입점
                 .userInfoEndpoint() // OAuth2 로그인 성공 이후 사용자 정보를 가져올 때의 설정들을 담당한다.
                 .userService(customOAuth2UserService); // 소셜 로그인 성공 시 후속 조치를 진행할 UserService 인터페이스의 구현체를 등록한다.
+
+        return http.build();
     }
 
 }
